2fc1ef8360dae0c88c427789f30c852e1c55166c
Jimmy <bruno_pier@hotmail.com>
2020-01-19 01:04:38 -0300

    Adding new component&code MoviesList.js

---
+++ src/components/MoviesList.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class MoviesList extends Component {
+    static propTypes = {
+        movies: PropTypes.array
+    }
+}
