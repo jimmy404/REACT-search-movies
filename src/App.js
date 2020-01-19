a118212b25e0fe27866bcdb82c21cd59c55b35be
Jimmy <bruno_pier@hotmail.com>
2020-01-19 00:19:14 -0300

    Adding _handleResults in App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import 'bulma/css/bulma.css'
 
 class App extends Component {
   state = {results: [] }
+
+  _handleResults = (results) => {
+    this.setState({ results })
+  }
+
   render() {
     return (
       <div className="App">
           <Title>Search Movies</Title>
         <div className='SearchForm-wrapper'>
-          <SearchForm />
+          <SearchForm onResults={this._handleResults}/>
         </div>
         {this.state.results.length === 0
         ? <p>Sin resultados</p>
