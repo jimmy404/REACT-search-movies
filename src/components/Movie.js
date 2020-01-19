ce35727aa598acabb5167dc0cc6e24c853404f05
Jimmy <bruno_pier@hotmail.com>
2020-01-19 00:53:10 -0300

    Adding new properties and fixing error - propTypes - Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Movie extends Component {
-    static PropTypes = {
+    static propTypes = {
         title: PropTypes.string,
         year: PropTypes.string,
         poster: PropTypes.string
@@ -15,14 +15,14 @@ export class Movie extends Component {
     <div className="card">
         <div className="card-image">
                 <figure className="image">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                    <img src={poster} alt={title}/>
                 </figure>
         </div>
             <div className="card-content">
                 <div className="media">
                 <div className="media-content">
-                        <p className="title is-4">John Smith</p>
-                        <p className="subtitle is-6">@johnsmith</p>
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6">{year}}</p>
                 </div>
             </div>
         </div>
