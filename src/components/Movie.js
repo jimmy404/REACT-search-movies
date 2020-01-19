dca16fcaa2e4ab2bed827d04ab08ef4b0b378ffa
Jimmy <bruno_pier@hotmail.com>
2020-01-19 02:47:01 -0300

    Project finished - Reordering code - Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+
 export class Movie extends Component {
     static propTypes = {
         id: PropTypes.string,
