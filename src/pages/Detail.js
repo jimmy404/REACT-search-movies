eefcc8d7888125bbfef274333f068db67ef216df
Jimmy <bruno_pier@hotmail.com>
2020-01-19 01:45:28 -0300

    Routing code - Detail.js

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class Detail extends Component {
+    static propTypes = {
+        id: PropTypes.string
+    }
+
+componentDidMount() {
+    const {id} = this.props
+    this._fetchMovie({id})
+}
+
     render() {
         return(
             <p>Pagina de detalle</p>
