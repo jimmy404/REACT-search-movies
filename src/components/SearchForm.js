dcad58ca585732c122812417b21c721bad1156f2
Jimmy <bruno_pier@hotmail.com>
2020-01-18 23:53:18 -0300

    Adding state code in SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 export class SearchForm extends Component {
+    state = {
+        inputMovie: ''
+    }
+
+    _handleChange = (e) => {
+        this.setState({inputMovie: e.target.value})
+    }
+
     render() {
         return(
             <div className="field has-addons">
                 <div className="control">
                     <input
                     className="input"
+                    onChange={this._handleChange}
                     type="text"
                     placeholder="Movie to search"
                     />
