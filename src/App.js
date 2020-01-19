9da1597da391eea4e43b9612b6464ef6611a12cf
Jimmy <bruno_pier@hotmail.com>
2020-01-19 02:02:02 -0300

    Adding new folder&file for Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import { Title } from './components/Title';
-import { SearchForm } from './components/SearchForm';
-import { MoviesList } from './components/MoviesList';
 
+
+import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
 import './App.css';
 import 'bulma/css/bulma.css'
 
 class App extends Component {
-  state = {usedSearch: false, results: [] }
-
-  _handleResults = (results) => {
-    this.setState({ results, usedSearch: true })
-  }
-
-  _renderResults() {
-    return this.state.results.length === 0
-    ? <p>Sorry! Results not found!</p>
-    : <MoviesList movies={this.state.results}/>
-    }
-
   render() {
     const url = new URL(document.location);
     const hasId = url.searchParams.has('id');
@@ -30,14 +17,7 @@ class App extends Component {
 
     return (
       <div className="App">
-          <Title>Search Movies</Title>
-        <div className='SearchForm-wrapper'>
-          <SearchForm onResults={this._handleResults}/>
-        </div>
-          {this.state.usedSearch
-          ? this._renderResults()
-          : <small>Use the form to search a movie</small>
-          }
+        <Home />
       </div>
     );
   }
