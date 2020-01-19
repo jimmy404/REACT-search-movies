fe9efedcb3762dc3d82c026697c641eb0903b5f3
Jimmy <bruno_pier@hotmail.com>
2020-01-19 02:30:38 -0300

    Adding button for 404 page - App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { NotFound } from './pages/NotFound';
 import './App.css';
 import 'bulma/css/bulma.css'
 
@@ -13,6 +14,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/detail/:movieId' component={Detail} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     );
