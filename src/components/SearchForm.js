0b750ec3095f42dd59aef31677a76ea1dba21236
Jimmy <bruno_pier@hotmail.com>
2020-01-18 23:56:56 -0300

    Reordering code SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -12,21 +12,21 @@ export class SearchForm extends Component {
     render() {
         return(
             <form onSubmit={this._handleChange}>
-            <div className="field has-addons">
+                <div className="field has-addons">
+                    <div className="control">
+                        <input
+                        className="input"
+                        onChange={this._handleChange}
+                        type="text"
+                        placeholder="Movie to search"
+                        />
+                    </div>
                 <div className="control">
-                    <input
-                    className="input"
-                    onChange={this._handleChange}
-                    type="text"
-                    placeholder="Movie to search"
-                    />
+                        <button className="button is-info">
+                        Search
+                        </button>
+                    </div>
                 </div>
-            <div className="control">
-                    <button className="button is-info">
-                    Search
-                    </button>
-                </div>
-            </div>
             </form>
         )
     }
